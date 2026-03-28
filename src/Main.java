447c5c3cbb4686e0f2bd336c69677835b1060441
Mikhail <myshkov07@gmail.com>
2026-03-28 11:21:32 +0300

    done Version2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -11,25 +13,50 @@ public class Main {
 
         int counter = 0;
 
-        boolean[] array = new boolean[1000000000];
+        int[] array = new int[n];
 
         for (int i = 0; i<n; i++) {
             int number = scanner.nextInt();
-            array[number-1] = true;
+            array[i] = number;
         }
 
-        int index = 0;
+        Map<Integer, Integer> goodNumbersMap = new HashMap<>();
 
-        while (counter!=k){
-            if (array[index]){
-                array[index+x] = true;
-                counter++;
+        for (int num: array){
+            int remains = num % x;
+
+            if (goodNumbersMap.containsKey(remains)){
+                if (remains < goodNumbersMap.get(remains)){
+                    goodNumbersMap.put(remains, num);
+                }
             }
-            index++;
+            else {
+                goodNumbersMap.put(remains, num);
+            }
+
         }
 
-        System.out.println(index);
+        int[] goodNums = goodNumbersMap.values().stream().mapToInt(i -> i).toArray();
 
+        int result1 = 0;
+        int result2 = 1000000000;
+
+        while (result1 != result2){
+            int count = 0;
+
+            for (int num : goodNums){
+                count += ((result2 + result1)/2 - num) / x;
+            }
+            count += goodNums.length;
+
+            if (count >= k){
+                result2 -= (result2 - result1)/2;
+            } else {
+                result1 += (result2 - result1)/2 +1;
+            }
+        }
+
+        System.out.println(result1);
 
     }
 }
